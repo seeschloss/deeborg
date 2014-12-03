a2dd881b4cc47350e555f6aa64e0166934a9d531
SeeSchloss <seeschloss@seos.fr>
2014-12-03 12:23:36 +0100

    Simple NodeJS server version of the bot

---
+++ deeborg-http.js
@@ -0,0 +1,65 @@
+// vim:et:sw=2
+/**
+ * Module dependencies.
+ */
+
+var http = require('http');
+var fs = require('fs');
+var deeborg = new (require('./deeborg.js').Bot)();
+
+var db_file = process.env.DATABASE || "deeborg.db";
+var port = process.env.PORT || 3000;
+var bind = process.env.BIND || "0.0.0.0";
+
+process.on('SIGINT', function() {process.exit();});
+process.on("exit", exit);
+
+function exit() {
+	console.log("Saving database...");
+	var db = deeborg.save();
+	fs.writeFileSync(db_file, db);
+	process.exit();
+}
+
+try {
+	var db = fs.readFileSync(db_file);
+	deeborg.load(db.toString());
+} catch (e) {
+	console.log("Database '" + db_file + "' does not exist,\nbot will not be able to answer much before it has learnt for a while.");
+}
+
+console.log("Listening on port " + port + "...");
+http.createServer(function(request, response) {
+	switch (request.method) {
+		case "POST":
+			var question = "";
+			request.on("data", function(data) {
+				question += data;
+
+				if (question.length > 1024) {
+					request.connection.destroy();
+				}
+			});
+			request.on("end", function() {
+				respondTo(question, request, response);
+			});
+			break;
+		case "GET":
+			var question = unescape(request.url.substr(2));
+			respondTo(question, request, response);
+			break;
+	}
+}).listen(port, bind);
+
+function respondTo(question, request, response) {
+	response.writeHead(200, {"Content-Type": "text/plain; charset=utf8"});
+	var answer = deeborg.answer(question);
+	response.write(answer + "\n");
+	response.end();
+
+	console.log("Answered '" + answer + "' to '" + question + "'");
+
+	deeborg.learn(question);
+	deeborg.organize();
+}
+
